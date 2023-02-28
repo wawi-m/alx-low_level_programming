6b092b94f8203be8894b79cd6a1d729846663a71
Gladys <wawigladys@gmail.com>
2023-02-28 01:32:11 -0800

    update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
@@ -18,5 +18,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		n++;
 	}
-	return (0);
 }
