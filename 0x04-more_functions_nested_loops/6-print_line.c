9aa02d0129ddbf663d3600564e932f572dde3d4d
Gladys <wawigladys@gmail.com>
2023-03-06 18:49:52 -0800

    create 6-10

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,21 @@
   * Return: void
   */
 void print_line(int n)
+void print_line(int n)
 {
-	int i;
+	int x;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-			_putchar(95);
+		_putchar('\n');
 	}
 	else
-	_putchar('\n');
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
 }
