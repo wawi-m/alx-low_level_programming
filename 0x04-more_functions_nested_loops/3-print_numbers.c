9cc17990e2f2fac8e01120eb11e6a0b6669ba453
Gladys <wawigladys@gmail.com>
2023-03-06 18:38:03 -0800

    create 3-5

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar((i % 10) + '0');
+
+	_putchar('\n');
+}
