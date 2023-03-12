e9c222a3bc2fba1de094299fb5ea5b54bc5c9ed5
Gladys <wawigladys@gmail.com>
2023-03-12 12:45:34 -0700

    update 3 & create 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * main - print the result of the multiplication, followed by a new line
  * @argc: int
  * @argv: list
- * Return: 0
+ * Return: 0 Always (success)
  */
 int main(int argc, char const *argv[])
 {
-	(void)argc;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
