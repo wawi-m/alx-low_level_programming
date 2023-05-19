fdb271084af68667ab9016a85c67ce194c748195
wawi <wawigladys@gmail.com>
2023-05-19 06:29:38 -0700

    complete mandatory tasks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'z'; lower >= 'a'; lower--)
+	{
+		putchar(lower);
+	}
+	putchar('\n');
+
+	return (0);
+}
