52bdfb57969259945d5aebdf65073145928b741a
wawi <wawigladys@gmail.com>
2023-05-30 19:47:49 -0700

    complete advanced tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - keygen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+
+	return (0);
+}
