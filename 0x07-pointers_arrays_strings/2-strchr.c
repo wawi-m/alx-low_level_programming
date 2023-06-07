ff2074d4cf5e6db16591c300aeea0ebd4246eea5
wawi <wawigladys@gmail.com>
2023-06-07 04:05:55 -0700

    complete mandatory tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != '\0' && *s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
 		s++;
 	}
+	if (*s == c)
+	{
+		return (s);
+	}
+
 	return (NULL);
 }
