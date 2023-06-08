716fbe3b5ee6a3f56b56c4943faa330c04f187f9
wawi <wawigladys@gmail.com>
2023-06-08 16:54:28 -0700

    complete advanced tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 #include <string.h>
+
+/**
+  * is_palindrome - Returns if a string is palindrome
+  * @s: the string value to be checked
+  *
+  * Return: integer value
+  */
 
 int is_palindrome(char *s)
 {
