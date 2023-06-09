d7b7c4f9c40f2b315219e12fc8fb1598515699b7
wawi <wawigladys@gmail.com>
2023-06-08 17:03:44 -0700

    complete advanced tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,16 +14,16 @@ int is_palindrome(char *s)
 
 	strcpy(s, r);
 	char *strrev(char *s);
-	
+
 	if (*s == '\0')
 		return (1);
 	else if (*s != '\0')
 	{
-		if (strcmp(s, r) == 0);
+		if (strcmp(s, r) == 0)
 			return (1);
 	}
 	else
 		return (0);
-	
+
 	return  (is_palindrome(s));
 }
